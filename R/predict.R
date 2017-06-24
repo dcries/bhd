a67e4b43ea5ecba8f6001a322a16581e5f22db40
unknown <dcries@GS.iastate.edu>
2017-06-24 18:06:04 -0500

    update

--- R/predict.R
+++ R/predict.R
@@ -3,13 +3,17 @@
 #' This function uses LASSO to select predictors in order to run linear regression to predict LDL for individuals. 
 #' Outputs a vector of predicted values of LDL corresponding on input
 #' There are no checks for NAs or anythingelse and thus this isa development level function.
-#' @param data Data.frame or matrix where the first column is what you want to predict, and remaining columns are what you want to consider as possibly covariates
+#' @param data Data.frame or matrix where the first column is what you want to predict, and 
+#' remaining columns are what you want to consider as possibly covariates. Any factors MUST be set as factors before 
+#' running. No character vectors allowed.
 #' @keywords preds
+#' @return A vector of length equal to number of rows of input data with predicted values of LDL
 #' @export
 #' @examples
 #' response <- rnorm(1000)
-#' predictors <- matrix(rnorm(10000),ncol=10)
-#' estimate.ldl(cbind(response,predictors))
+#' goodpredictors <- 0.5*response + rnorm(1000,0.5)
+#' badpredictors <- matrix(rnorm(10000),ncol=10)
+#' estimate.ldl(cbind(response,goodpredictors,badpredictors))
 
 
 estimate.ldl <- function(data){
@@ -17,6 +21,9 @@ estimate.ldl <- function(data){
   x <- as.matrix(data[,2:nc])
   m1 <- glmnet::cv.glmnet(x,data[,1])
   keep <- ((as.numeric((coef(m1, s = "lambda.1se"))))!=0)[-1]
+  if(sum(keep)==0){
+    return("No variables appeared to be important, no predictions made")
+  }
   #xreduced <- x[,which(coef(m1, s = "lambda.1se") !=0)[-1]-1]
   xreduced <- x[,keep]
   m2 <- lm(data[,1]~xreduced)
